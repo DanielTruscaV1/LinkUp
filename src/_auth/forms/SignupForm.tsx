d052ac4d8d2fd0a815a1e475145a59cde97150be
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-05-07 01:23:36 +0300

    Update V1.

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+const SignupForm = () => {
+  return (
+    <div>
+      <Button>
+
+      </Button>
+    </div>
+  )
+}
+
+export default SignupForm
