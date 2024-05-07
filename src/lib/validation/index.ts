59ef358aa707488cc687a58fd49b35d6e1f93a5d
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-05-07 15:42:44 +0300

    Update V2.

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import { z } from "zod"
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, { message: "Too short."}),
+    username: z.string().min(2, { message: "Too short."}).max(50),
+    email: z.string().email(),
+    password: z.string().min(8, { message: "Too short."}),
+  })
